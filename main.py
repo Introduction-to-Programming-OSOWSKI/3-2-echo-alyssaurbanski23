10b770352a20be54fdc7b9721457af03379eac52
alyssaurbanski23 <831554@mystma.org>
2021-03-07 22:24:21 +0000

    echo try 3

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ def echo(word, x):
 
         newWord = word
 
-        for i in range(0, x - 1):
+        for i in range(0, x + 1):
             newWord = newWord = word
 
         return newWord
 
-print (echo("yo", 10))
+print (echo("yo", 2))
